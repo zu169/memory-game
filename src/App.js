9f527e0b7d065772b4ef1a23517a0a845c965d24
zu169 <zzuzanna.03@gmail.com>
2023-10-25 17:28:47 +0100

    create project repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+const cardImages = [
+  {"src":"/img/helmet-1.png"},
+  {"src":"/img/potion-1.png"},
+  {"src":"/img/ring-1.png"},
+  {"src":"/img/scroll-1.png"},
+  {"src":"/img/shield-1.png"},
+  {"src":"/img/sword-1.png"},
+]
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random()}))
+
+    setCards(shuffleCards)
+    setTurns(0)
+  }
+
+  console.log(cards,turns)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Kana Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div cardName="card-grid">
+        {cards.map(card => (
+          <div key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.png" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
